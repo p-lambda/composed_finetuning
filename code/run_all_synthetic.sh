f8ad808af9476043178a65ea26868ec9a9c528b9
Michael Xie <sangxie513@gmail.com>
2021-12-25 15:22:19 -0600

    runners

--- code/run_all_synthetic.sh
+++ code/run_all_synthetic.sh
@@ -6,7 +6,7 @@ num_examples=1000
 num_unlabeled=20000
 dropout=0.1
 suffix=a
-num_layers=6
+num_layers=3
 save_root=save_root
 mode=direct,denoise,pretrain,pretrain+compose
-python run_synthetic.py --num_examples ${num_examples} --num_unlabeled ${num_unlabeled} --dropout ${dropout} --suffix ${suffix} --num_layers ${num_layers} --save_root ${save_root} --mode ${mode} --eval_only
+python run_synthetic.py --num_examples ${num_examples} --num_unlabeled ${num_unlabeled} --dropout ${dropout} --suffix ${suffix} --num_layers ${num_layers} --save_root ${save_root} --mode ${mode}
